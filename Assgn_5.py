53f2d89677f41914f270901c6e65d1fd3d32bf50
RoshanatNiser <roshan.yadav@niser.ac.in>
2025-09-03 02:04:25 +0530

    Update Assgn_5.py

--- Assgn_5.py
+++ Assgn_5.py
@@ -8,8 +8,8 @@ from Func_lib_assgn_5 import *
 print("Solving Question 1")
 A=read_matrix('A.txt')
 b=read_vector('b.txt')
-print("\nOutput of Cholesky decomposition",cholesky_solve(A,b))
-print("\nOutput of Jacobi:",jacobi(A,b))
+print("\nOutput of Cholesky decomposition",cholesky_solve(A,b)) #output:[1.0, 0.9999999999999999, 1.0, 1.0, 1.0, 1.0]
+print("\nOutput of Jacobi:",jacobi(A,b))                        #output:[0.9999989753998146, 0.9999985509965219, 0.9999989753998146, 0.9999989753998146, 0.9999985509965219, 0.9999989753998146]
 
 
 
@@ -17,6 +17,5 @@ print("\nOutput of Jacobi:",jacobi(A,b))
 print("\nSolving Question 2")
 C=read_matrix('C.txt')
 d=read_vector('d.txt')
-print("\nOutput of Jacobi:",jacobi(C,d))
-print("\n Output of gauss-seidel",gauss_seidel(C,d))
-
+print("\nOutput of Jacobi:",jacobi(C,d))             #output:[2.9791649583226008, 2.215599258220273, 0.21128373337161171, 0.15231661140963978, 5.71503326456748]
+print("\n Output of gauss-seidel",gauss_seidel(C,d)) #output:[2.979165086347139, 2.215599676186742, 0.21128402698819165, 0.15231700827754793, 5.715033568811629]
